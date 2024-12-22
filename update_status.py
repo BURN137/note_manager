ec7bd28ecf2e5c90e38cdee63e4f8c7e6b21b2ff
BURN137 <aiarslanov@gmail.com>
2024-12-22 20:47:42 +0500

    Grade 1. Этап 2: Задание 2

--- update_status.py
+++ update_status.py
@@ -1,45 +1,55 @@
-# status = "Активна"  # Статус заметки
-# выполнено
-# в процессе
-# отложено
+# Запрашиваем текущий статус заметки с приведением статусов в вид "Выполнено", "В процессе", "Отложено"
 print("\nВозможные статусы заметки:"
-          "\n1. Выполнено"
-          "\n2. В процессе"
-          "\n3. Отложено")
-status = input("Введите статус заметки (1/2/3): ")
+          "\nВыполнено"
+          "\nВ процессе"
+          "\nОтложено")
+status = input("Введите статус заметки: ")
 while True:
-    if status == "1":
-        print("\nТекущий статус заметки: Выполнено")
+    if status.lower() == "выполнено":
+        status = status.lower().capitalize()
+        print("\nТекущий статус заметки:", status)
         break
-    elif status == "2":
-        print("\nТекущий статус заметки: В процессе")
+    elif status.lower() == "в процессе":
+        status = status.lower().capitalize()
+        print("\nТекущий статус заметки:", status)
         break
-    elif status == "3":
-        print("\nТекущий статус заметки: Отложено")
+    elif status.lower() == "отложено":
+        status = status.lower().capitalize()
+        print("\nТекущий статус заметки:", status)
         break
     else:
         print("\nВозможные статусы заметки:"
-              "\n1. Выполнено"
-              "\n2. В процессе"
-              "\n3. Отложено")
-        status = input("Введите корректно статус заметки (1/2/3): ")
+              "\nВыполнено"
+              "\nВ процессе"
+              "\nОтложено")
+        status = input("Введите корректно статус заметки: ")
+
+# Смена статуса заметки при необходимости
 status_changes = input("Хотите изменить статус заметки? (Да/Нет) ")
 while True:
     if status_changes.lower() == "да":
-        new_status = input("Введите новый статус заметки (1/2/3): ")
-        if new_status == "1":
-            print("Статус заметки успешно обновлен на: Выполнено")
+        new_status = input("Введите новый статус заметки: ")
+        if new_status.lower() == "выполнено":
+            new_status = new_status.lower().capitalize()
+            print("Статус заметки успешно обновлен на:", new_status)
             break
-        elif new_status == "2":
-            print("Статус заметки успешно обновлен на: В процессе")
+        elif new_status.lower() == "в процессе":
+            new_status = new_status.lower().capitalize()
+            print("Статус заметки успешно обновлен на:", new_status)
             break
-        elif new_status == "3":
-            print("Статус заметки успешно обновлен на: Отложено")
+        elif new_status.lower() == "отложено":
+            new_status = new_status.lower().capitalize()
+            print("Статус заметки успешно обновлен на:", new_status)
             break
+        else:
+            print("\nДанный статус отсутствует")
     elif status_changes.lower() == "нет":
         new_status = status
         print("Текущий статус заметки не изменен")
         break
     else:
         status_changes = input("Пожалуйста, введите 'Да' или 'Нет': ")
-print({"status": new_status})
+
+# Создание словаря и его вывод
+status_ = {"status": new_status}
+print(status_)
